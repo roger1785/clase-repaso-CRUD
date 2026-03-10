5275bf9c3c94f673ea48372a8e459561ed510315
roger17 <roger17@git.com>
2026-03-10 20:23:42 +0100

    commit

---
+++ routes/task.router.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const router = Router();
+
+import Task from "../models/Task";
+
+router.get("/task", async (req, res) => {
+  const task = await Task.find();
+});
+
+export default router;
